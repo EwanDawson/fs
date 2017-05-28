0f98e82581831b176d49bcd233938ce57b8a322b
Ewan Dawson <ewan.dawson@symphonicsoft.com>
2017-05-28 23:26:53 +0100

    Upgraded Clojure to 1.8.0. Added deps for CIDER

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/Raynes/fs"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.commons/commons-compress "1.8"]]
   :plugins [[lein-midje "3.1.3"]
-            [codox "0.8.10"]]
+            [codox "0.8.10"]
+            [cider/cider-nrepl "0.15.0-SNAPSHOT"]]
   :codox {:src-dir-uri "https://github.com/Raynes/fs/blob/master/"
           :src-linenum-anchor-prefix "L"
           :defaults {:doc/format :markdown}}
